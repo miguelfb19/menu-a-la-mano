131c1a013d9f04b01e31ecabd06f596d37a4401c
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-11-21 12:12:10 -0500

    add favicon in /app/page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
 import { redirect } from "next/navigation";
 
+
+export const metadata = {
+ title: 'Menú a la mano',
+ description: 'Menú a la mano',
+ icons: {
+  icon: '/favicon.svg',
+ }
+};
+
 export default function Home() {
 
   redirect('/home')
