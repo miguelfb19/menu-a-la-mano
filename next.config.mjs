04735c7a84037018317db725ab5de042136d39a9
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2024-11-23 12:26:48 -0500

    add headers to no- iframes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "frame-ancestors 'none'",
+                        // Con esto evitamos incrustación de nuestro sitio en otro sitio
+                    },
+                ],
+            },
+        ];
+    },
     images: {
         remotePatterns: [
             {
